51e334c389f9d8f66744bdffc5909172b0f64ea4
74lg0 <111157836+74lg0@users.noreply.github.com>
2022-12-31 19:51:39 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+pip install colorama -y
+
+apt install curl wget -y
+
+apt install curl -y
+
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+
+chmod 755 msfinstall
+
+./msfinstall
