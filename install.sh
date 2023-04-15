44a138fa6c0d11bb37c6a29aedca345d69f5dc19
74lg0 <111157836+74lg0@users.noreply.github.com>
2023-04-14 22:15:20 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,3 @@ apt install curl -y
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 
 chmod 755 msfinstall
-
-./msfinstall
