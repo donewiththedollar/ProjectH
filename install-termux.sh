4e2aaa0381a8b6a6e32e148b58cfd48fc606bc4e
74lg0 <111157836+74lg0@users.noreply.github.com>
2023-05-03 10:01:31 -0600

    Create install-termux.sh

---
+++ install-termux.sh
@@ -0,0 +1,12 @@
+echo -e "Iniciando Instalacion..."
+pip install colorama -y
+
+pkg install curl wget -y
+
+pkg install curl -y
+
+wget https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
+
+chmod +x metasploit.sh
+
+echo -e "Instalacion Terminada..."
