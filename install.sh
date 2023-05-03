bb1e637b9653bc0b52983fb478c9d7fa8a41afef
74lg0 <111157836+74lg0@users.noreply.github.com>
2023-05-03 09:57:40 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 pip install colorama
 
-apt install curl wget -y
+sudo apt install curl wget -y
 
-apt install curl -y
+sudo apt install curl -y
 
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 
