6749b2d30c1a2c61f42ec8eac2c961612029b7d4
74lg0 <111157836+74lg0@users.noreply.github.com>
2023-05-07 15:54:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ sudo apt install curl wget -y
 
 sudo apt install curl -y
 
+chmod +x MiniLav.py
+
+chmod +x astronet.py
+
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 
 chmod 755 msfinstall
